afe127bdd1b600c58d3f69eba71e39289a53a5ef
Maria Leonova <leonova-99@list.ru>
2020-01-10 15:55:05 +0300

    Update LockData.js

--- src/api/LockData.js
+++ src/api/LockData.js
@@ -9,7 +9,7 @@ export const sendLockData= (person, design, size, message) => {
         message: message
     };
     return axios
-        .post('http://localhost:5000/api/send_lock_data', payload)
+        .post('https://localhost:5000/api/send_lock_data', payload)
         .then(res => {
             console.log('Данные успешно были отправлены');
             return res.data;
